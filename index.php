a1a25db0c9f56826989e2ab1b678c46b0c379df4
Maxime DEFRETIN <maxime.defretin@laposte.net>
2017-12-12 14:27:17 +0100

    exercice

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+$IsEasy = false;
+if ($IsEasy === true) {
+  echo "C'est facile !!";
+}
+else {
+  echo "C'est difficile !!!";
+}
+/*$easy = "C'est facile !!";
+$difficult = "C'est difficile !!!";
+switch ($IsEasy) {
+  case 'true':
+  echo $easy;
+  break;
+
+  case 'false':
+  echo $difficult;
+  break;
+}*/
+?>
