19a1711302458dfd48e36f3d93c77fed4730c83d
Maxime DEFRETIN <maxime.defretin@laposte.net>
2017-12-12 14:56:40 +0100

    exercice

--- index.php
+++ index.php
@@ -1,20 +1,19 @@
-<?php
-$IsEasy = false;
-if ($IsEasy === true) {
-  echo "C'est facile !!";
-}
-else {
-  echo "C'est difficile !!!";
-}
-/*$easy = "C'est facile !!";
-$difficult = "C'est difficile !!!";
-switch ($IsEasy) {
-  case 'true':
-  echo $easy;
-  break;
-
-  case 'false':
-  echo $difficult;
-  break;
-}*/
-?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="style.css">
+  <title>Partie 2 : Exercice 2</title>
+</head>
+<body>
+  <?php
+  $IsEasy = false;
+  if ($IsEasy === true) {
+    echo "C'est facile !!";
+  }
+  else {
+    echo "C'est difficile !!!";
+  }
+  ?>
+</body>
+</html>
